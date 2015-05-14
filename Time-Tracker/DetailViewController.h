7e542969747205bd8aeee907c957d699ae2c4e7e
Daniel Dayley <danieldayley@aim.com>
2015-05-14 15:36:50 -0600

    Add and Remove Entries methods added

---
+++ Time-Tracker/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  Time-Tracker
+//
+//  Created by Daniel Dayley on 5/14/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
